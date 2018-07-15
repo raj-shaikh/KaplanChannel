95dc29395c59ded19f1d652f604041d52c0c6f1c
CaptainUI <raj.shaikh@majesco.com>
2018-07-15 00:45:31 -0400

    Added timezone in channel tile

--- src/app/components/channel-tile/channel-tile.component.ts
+++ src/app/components/channel-tile/channel-tile.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
 import {IChannel} from "../../app.component";
+import * as moment from 'moment';
+import * as mtz from "moment-timezone";
 
 @Component({
   selector: 'app-channel-tile',
@@ -11,9 +13,15 @@ export class ChannelTileComponent implements OnInit {
   @Input('channel')
   channel:IChannel;
 
+  public formattedTime: string;
+
   constructor() { }
 
   ngOnInit() {
+    const endTime = moment(this.channel.time).add(1, 'hour');
+    const startMoment = moment(this.channel.time).format('h:mm A');
+    const endMoment = moment(endTime).format('h:mm A');
+    this.formattedTime = `${startMoment} - ${endMoment} ${mtz.tz.zone(mtz.tz.guess()).abbr(moment.now())}`;
   }
 
 }
