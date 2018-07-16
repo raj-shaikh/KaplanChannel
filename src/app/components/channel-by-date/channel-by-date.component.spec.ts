77f31c6dbf3bbf48e81c509680d7975650225f16
CaptainUI <raj.shaikh@majesco.com>
2018-07-16 07:24:19 -0400

    Updated test cases

--- src/app/components/channel-by-date/channel-by-date.component.spec.ts
+++ src/app/components/channel-by-date/channel-by-date.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChannelByDateComponent } from './channel-by-date.component';
+import {AppModule} from "../../app.module";
 
 describe('ChannelByDateComponent', () => {
   let component: ChannelByDateComponent;
@@ -8,7 +9,10 @@ describe('ChannelByDateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChannelByDateComponent ]
+      declarations: [  ],
+      imports: [
+        AppModule
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +20,22 @@ describe('ChannelByDateComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChannelByDateComponent);
     component = fixture.componentInstance;
+    component.channels = [{
+      "title":"Nulla convallis dolor quis erat.",
+      "description":"Sed hendrerit luctus finibus. Sed justo dui, vulputate ac suscipit condimentum, porttitor sed dolor. Ut eu justo at metus dapibus facilisis a quis libero. Integer lectus turpis, pretium a tincidunt.",
+      "instructorName":"Erat Libero",
+      "instructorPhotoUrl":"https://placeholdit.imgix.net/~text?txtsize=34&txt=C&w=60&h=60",
+      "subjectPhotoUrl":"https://placeholdit.imgix.net/~text?txtsize=34&txt=C&w=60&h=60",
+      "time":"2016-01-03 22:00:00"
+    }];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have display date', () => {
+    expect(component.formattedDate).toEqual('Sun, January 03, 2016');
   });
 });
--- src/app/components/channel-tile/channel-tile.component.spec.ts
+++ src/app/components/channel-tile/channel-tile.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChannelTileComponent } from './channel-tile.component';
+import {AppModule} from "../../app.module";
 
 describe('ChannelTileComponent', () => {
   let component: ChannelTileComponent;
@@ -8,7 +9,11 @@ describe('ChannelTileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ChannelTileComponent ]
+      declarations: [ ],
+      imports: [
+        AppModule
+
+      ]
     })
     .compileComponents();
   }));
@@ -16,10 +21,22 @@ describe('ChannelTileComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ChannelTileComponent);
     component = fixture.componentInstance;
+    component.channel = {
+      "title":"Nulla convallis dolor quis erat.",
+      "description":"Sed hendrerit luctus finibus. Sed justo dui, vulputate ac suscipit condimentum, porttitor sed dolor. Ut eu justo at metus dapibus facilisis a quis libero. Integer lectus turpis, pretium a tincidunt.",
+      "instructorName":"Erat Libero",
+      "instructorPhotoUrl":"https://placeholdit.imgix.net/~text?txtsize=34&txt=C&w=60&h=60",
+      "subjectPhotoUrl":"https://placeholdit.imgix.net/~text?txtsize=34&txt=C&w=60&h=60",
+      "time":"2016-01-03 22:00:00"
+    };
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have display time', () => {
+    expect(component.formattedTime).toEqual('10:00 PM - 11:00 PM EDT');
   });
 });
